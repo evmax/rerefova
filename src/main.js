43e581f65f3ee785ec6ad560b008f58d4a5c33d2
Aleksey Pirogov <astynax@users.noreply.github.com>
2016-08-10 22:05:54 +0300

    add validation (with validate.js)

--- src/main.js
+++ src/main.js
@@ -3,12 +3,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider, connect } from 'react-redux';
 import R from 'ramda';
+import validate from 'validate.js';
+
+// validation
+
+const nameFormat = {
+    pattern: /^[A-Za-z]{2,}$/,
+    message: value => `^Name should contain only letters (two or more)!`
+};
+
+const constraints = {
+    first: {
+        presence: {message: '^First name can\'t be empty!'},
+        format: nameFormat
+    },
+    last: {
+        format: nameFormat
+    },
+    age: {
+        presence: true,
+        numericality: {
+            onlyInteger: true,
+            greaterThanOrEqualTo: 18
+        }
+    }
+};
 
 // reducer and store
 
 const initialState = {
     first: '',
-    last: ''
+    last: '',
+    age: ''
 };
 
 const reducer = (state, action) => {
@@ -24,9 +50,9 @@ const store = createStore(reducer, initialState);
 
 // form component
 
-const mapStateToProps = ({ first, last }) => ({
-    first, last,
-    fullname: `${first} ${last}`
+const mapStateToProps = ({ first, last, age }) => ({
+    first, last, age,
+    errors: validate({ first, last, age }, constraints) || {}
 });
 
 const mapDispatchToProps = dispacth => ({
@@ -34,24 +60,33 @@ const mapDispatchToProps = dispacth => ({
                                         value: event.target.value})
 });
 
+const Field = ({params: [label, value, field, errors, callback]}) => (
+    <div>
+      {label}:
+      <br/>
+      <input onChange={callback(field)}
+             value={value}
+             style={R.has(field, errors) ? {color: 'red'} : {}}
+             />
+      <span style={{color: 'red'}}>
+        {(errors[field] || []).join(',')}
+      </span>
+    </div>
+);
+
 class FormClass extends React.Component {
     render () {
-        const { first, last, fullname, update } = this.props;
+        const { first, last, age, errors, update } = this.props;
         return (
             <form>
               <fieldset>
                 <legend>Personal information:</legend>
-                First name:
-                <input type="text"
-                       onChange={update('first')}
-                       value={first}/>
-                <br/>
-                Last name:
-                <input type="text"
-                       onChange={update('last')}
-                       value={last}/>
-                <br/>
-                Fullname: {fullname}
+
+                <Field params={['First name', first, 'first', errors, update]} />
+                <Field params={['Last name', last, 'last', errors, update]} />
+                <Field params={['Age', age, 'age', errors, update]} />
+
+                Hello, {`${first} ${last} (${age})`}
               </fieldset>
             </form>
         );
@@ -62,7 +97,6 @@ const Form = connect(
     mapStateToProps,
     mapDispatchToProps
 )(FormClass);
-
 
 // rendering
 
